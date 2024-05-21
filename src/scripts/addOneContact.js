2289cf72bf9657087215a84d0ce02074be1f98bb
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-21 23:27:49 +0300

    added-addOneContact-logics

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const fileData = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(fileData);
+
+    const newContact = createFakeContact();
+
+    const updatedContacts = [...contacts, newContact];
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error('Помилка при генерації контакта:', error);
+  }
+};
 
 await addOneContact();

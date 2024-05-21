758a26924e06b93ea7a2a271e83a9d397eacf756
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-21 16:30:15 +0300

    added-generateContacts-logics

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const fileData = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(fileData);
+
+    const newContacts = Array.from({ length: number }, () =>
+      createFakeContact(),
+    );
+
+    const updatedContacts = [...contacts, ...newContacts];
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error('Помилка при генерації контактів:', error);
+  }
+};
 
 await generateContacts(5);

765e679bdcb1023fb97d3567765101b4d92cc20f
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-05-24 14:32:05 +0300

    added-thanos-logics

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const fileData = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(fileData);
+    const updatedContacts = contacts.filter(() => Math.random() >= 0.5);
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+  } catch (error) {
+    console.error('Помилка при виділенні контактів:', error);
+  }
+};
 
 await thanos();
